7fc752309de3ae3130489c1417b42f943c7d5327
Bohdan Semeniuk <48619993+bohdansem1@users.noreply.github.com>
2023-12-03 16:32:59 +0200

    Add files via upload

---
+++ lesson_18/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let burger = document.querySelector('.burger-menu');
+
+// burger.addEventListener('click', () =>{
+//    console.log('1'); /* працює */
+// });
+
+burger.onclick = () => {
+   // console.log('1');/*  працює */
+  document.documentElement.classList.toggle('burger-open');
+};
